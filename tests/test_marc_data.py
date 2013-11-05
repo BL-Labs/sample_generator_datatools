60b514a49c4eac374fb6f86126ca2a22d5018669
Ben O'Steen <bosteen@gmail.com>
2013-11-05 12:28:56 +0000

    Completed coverage. Added solr dict creating function

--- tests/test_marc_data.py
+++ tests/test_marc_data.py
@@ -6,14 +6,18 @@ import unittest, pymarc, os
 from utils.marcutils import *
 
 TF1 = "tests/single.xml"
+TF2 = "tests/multiple.xml"
 
 class TestMARCParsing(unittest.TestCase):
   def setUp(self):
     self.tf1 = pymarc.parse_xml_to_array(TF1)
     self.collated_tf1 = collate(self.tf1[0])
+    self.tf2 = pymarc.parse_xml_to_array(TF2)
+    self.collated_tf2 = collate(self.tf2[0])
 
   def test_validate_testdata(self):
     self.assertEqual(len(self.tf1), 1)
+    self.assertEqual(len(self.tf2), 8)
 
   def test_collate(self):
     self.assertTrue("001" in self.collated_tf1)
@@ -21,6 +25,10 @@ class TestMARCParsing(unittest.TestCase):
     self.assertTrue("852" in self.collated_tf1)
     self.assertTrue("245" in self.collated_tf1)
     self.assertTrue("260" in self.collated_tf1)
+    self.assertTrue("001" in self.collated_tf2)
+    self.assertTrue("008" in self.collated_tf2)
+    self.assertTrue("852" in self.collated_tf2)
+    self.assertTrue("245" in self.collated_tf2)
     self.assertTrue(len(self.collated_tf1['852']), 2)
     
   def test_tf1_001(self):
@@ -54,6 +62,71 @@ class TestMARCParsing(unittest.TestCase):
   def test_tf1_get_titles(self):
     expected = [u'Tres Imposibles Vencidos, o\u0301 tres inventos del siglo. La direccion de los globos. El movimiento continuo. La cuadratura del ci\u0301rculo. Resolucion y demostracion cienti\u0301fica de estos tres problemas. [The dedication signed: F. G. A. With diagrams.]']
     self.assertTrue(len(self.collated_tf1['245']) == 1 and get_titles(self.collated_tf1) == expected)
+
+  def test_tf1_get_pub_detail(self):
+    expected = [u"p/Barcelona,", u"", u"p/1864."]
+    self.assertTrue(len(self.collated_tf1['260']) == 1 and get_pub_detail(self.collated_tf1) == expected) 
+
+  def test_tf1_get_phys_desc(self):
+    expected = [u'55 p. ;', u'8\xba.']
+    self.assertTrue(len(self.collated_tf1['300']) == 1 and get_phys_desc(self.collated_tf1) == expected)
+  
+  def test_tf1_get_general_note(self):
+    expected = []
+    self.assertTrue(len(self.collated_tf1['500']) == 0 and get_general_note(self.collated_tf1) == expected)
+
+  def test_tf1_get_domids(self):
+    expected = []
+    self.assertTrue(len(self.collated_tf1['852']) == 1 and get_domids(self.collated_tf1) == expected)
+
+  def test_tf1_get_shelfmarks(self):
+    expected = [u'British Library HMNTS 8706.ee.21.(4.)']
+    self.assertTrue(len(self.collated_tf1['852']) == 1 and get_shelfmarks(self.collated_tf1) == expected)
+
+  def test_tf2_001(self):
+    # 000000523
+    self.assertTrue(len(self.collated_tf2["001"]) == 1 and self.collated_tf2["001"][0].value() == u'014811575')
+
+  def test_tf2_get_sysnum(self):
+    self.assertTrue(len(self.collated_tf2["001"]) == 1 and get_sysnum(self.collated_tf2) == u'014811575')
+
+  def test_tf2_008_lang(self):
+    self.assertTrue(len(self.collated_tf2["008"]) == 1 and self.collated_tf2["008"][0].value()[35:38] == u"und")
+
+  def test_tf2_get_lang(self):
+    self.assertTrue(len(self.collated_tf2["008"]) == 1 and get_lang(self.collated_tf2) == u"und")
+
+  def test_tf2_get_names(self):
+    expected = {'personal': [u'author/Gibbon, Charles, 1843-1890.'], 'corporate': []}
+    self.assertTrue(len(self.collated_tf2["100"]) == 1 and get_names(self.collated_tf2) == expected)
+
+  def test_tf2_get_facetnames(self):
+    expected = {'personal': [u'author/Gibbon,_Charles,__1843-1890.'], 'corporate': []}
+    self.assertTrue(len(self.collated_tf2["100"]) == 1 and get_names(self.collated_tf2, facet = True) == expected)
+
+  def test_tf2_get_titles(self):
+    expected = [u'Dangerous Connexions. A novel. [electronic resource]']
+    self.assertTrue(len(self.collated_tf2['245']) == 1 and get_titles(self.collated_tf2) == expected)
+
+  def test_tf2_get_pub_detail(self):
+    expected = [u'p/London,', u'', u'p/1864.']
+    self.assertTrue(len(self.collated_tf2['260']) == 1 and get_pub_detail(self.collated_tf2) == expected) 
+
+  def test_tf2_get_phys_desc(self):
+    expected = [u'3 vol. ;', u'8\xba.']
+    self.assertTrue(len(self.collated_tf2['300']) == 1 and get_phys_desc(self.collated_tf2) == expected)
+  
+  def test_tf2_get_general_note(self):
+    expected = [u'Testing General Note']
+    self.assertTrue(len(self.collated_tf2['500']) == 1 and get_general_note(self.collated_tf2) == expected)
+
+  def test_tf2_get_domids(self):
+    expected = [u'lsidyv3b0a7b28', u'lsidyv3aee4429', u'lsidyv3aee4b76']
+    self.assertTrue(len(self.collated_tf2['852']) == 3 and get_domids(self.collated_tf2) == expected)
+
+  def test_tf2_get_shelfmarks(self):
+    expected = []
+    self.assertTrue(len(self.collated_tf2['852']) == 3 and get_shelfmarks(self.collated_tf2) == expected)
 
 if __name__ == '__main__':
   unittest.main()
--- utils/marcutils.py
+++ utils/marcutils.py
@@ -106,3 +106,99 @@ def get_names(collated_record, facet = False):
   else:
     return {'personal': map(flatten_name, names['100']),
             'corporate': map(flatten_name, names['110'])}
+
+def get_titles(collated_record):
+  # A title can hide in 245 $a + $b, 240 and 130 on occasion.
+  # ~99.9% of records had a main title in 245
+  # and 240 + 130 coverage was below 15% so skipping for now
+  # Output is still as a list, in case this changes
+  if collated_record.get('245', u""):
+    maintitles = [x.value() for x in collated_record['245'] + collated_record['240'] + collated_record['130']]
+    return maintitles
+  else:
+    return u""
+
+def get_pub_detail(collated_record):
+  # 260 $a Place of Publication/Distribution
+  #     $b Name of Publisher/Distrib
+  #     $c date of Pub
+  #     $e Place of Manuf
+  #     $f manufacturer
+  #     $g Manuf date
+  #  Near 95% coverage in the dataset
+  if collated_record.get("260", u""):
+    # Typically all contained in a single field.
+    pubfield = collated_record['260'][0]
+    pdtype = u"m"
+    place = pubfield.get_subfields("e")
+    date = pubfield.get_subfields("f")
+    maker = pubfield.get_subfields("g")
+    if pubfield.get_subfields("a"):
+      pdtype = u"p"
+      place = pubfield.get_subfields("a")
+      date = pubfield.get_subfields("c")
+      maker = pubfield.get_subfields("b")
+    def present_value(items):
+      if len(items[0]) == 1:
+        return u"{0}/{1}".format(items[1], items[0][0])
+      return u""
+
+    return map(present_value, [(place, pdtype), (maker, pdtype), (date, pdtype)])
+
+def get_phys_desc(collated_record):
+  # $a - Extent (R)
+  # $b - Other physical details (NR)
+  # $c - Dimensions (R)
+  # $e - Accompanying material (NR)
+  # $f - Type of unit (R)
+  # $g - Size of unit (R)
+  # $3 - Materials specified (NR)
+  # $6 - Linkage (NR)
+  # $8 - Field link and sequence number (R)
+  # Lump it all in there? 
+  def iter_subf(fields):
+    for x in fields:
+      for y in x.get_subfields("a", "b", "c", "e", "f", "g", "3", "6"):
+        yield y
+  if collated_record.get("300"):
+    return  [y for y in iter_subf(collated_record["300"])]
+  return []
+
+def get_general_note(collated_record):
+  if collated_record.get("500"):
+    return [x.value() for x in collated_record['500']]
+  return []
+
+def get_domids(collated_record):
+  if collated_record.get("852"):
+    sfx = filter(lambda x: x.get_subfields("c") == [u"SFX"], collated_record["852"])
+    if sfx:
+      domids = [x.get_subfields("j")[0] for x in sfx if x.get_subfields("j") and x.get_subfields("j")[0].startswith("lsid")]
+      return domids
+  return []
+
+def get_shelfmarks(collated_record):
+  # ignore SFX + lsid shelfmarks, as these are harvested by the get_domids part
+  marks = []
+  if collated_record.get("852"):
+    for sm in collated_record['852']:
+      if not(sm.get_subfields("c") == [u"SFX"] and sm.get_subfields("j")[0].startswith("lsid")):
+        marks.append(sm.value())
+  return marks
+
+def get_solr_doc(collated_record):
+  names = get_names(collated_record)
+  pubplace, maker, pubdate = get_pub_detail(collated_record)
+  doc = {'id': get_sysnum(collated_record),
+         'title': get_titles(collated_record),
+         'personal': names['personal'],
+         'corporate': names['corporate'],
+         'place': pubplace,
+         'maker': maker,
+         'date': pubdate,
+         'physdesc': get_phys_desc(collated_record),
+         'general': get_general_note(collated_record),
+         'domids': get_domids(collated_record),
+         'shelfmarks': get_shelfmarks(collated_record),
+         'lang': get_lang(collated_record)}
+  return doc
